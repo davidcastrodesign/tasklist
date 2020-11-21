e93a62adcc760c99c96e7cc05f0b8de3288a272b
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-21 01:34:11 -0800

    load events & add tasks function added

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a Task');
+  }
+
+  e.preventDefault();
+}
