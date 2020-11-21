f8aefa83ee6d14d05a37c67ba6860643c886b04a
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-21 15:45:34 -0800

    task li append added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 // Load all event listeners
+// instead of using submit on global scope, submit is being use under loadeventListeners function
 loadEventListeners();
 
 // load all event listeners
@@ -19,5 +20,27 @@ function addTask(e) {
     alert('Add a Task');
   }
 
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = '';
+
+  // Used to stop default form-submit behavior
   e.preventDefault();
 }
