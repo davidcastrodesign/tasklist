1ff1a02a9458b660cc990385b2290650972daefa
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-22 13:03:56 -0800

    forEach loop added to removeFromLS func

--- app.js
+++ app.js
@@ -121,6 +121,14 @@ function removeTaskFromLocalStorage(taskItem) {
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Clear Tasks
