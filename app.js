0672af4d8e8e7b425dfcd1cc5cd744a58e8a430c
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-22 10:54:08 -0800

    remove task from LS added to remove task func

--- app.js
+++ app.js
@@ -106,6 +106,9 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
     if (confirm('Are You Sure?')) {
       e.target.parentElement.parentElement.remove();
+
+      // Remove from LS
+      removeTaskFromLocalStorage();
     }
   }
 }
