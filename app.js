c20c036fb5013338b017e9ffe73dce37610a5fd9
David Castro Castaneda <dcastrocast@gmail.com>
2020-11-22 10:42:25 -0800

    forEach For LS added into getTasks function

--- app.js
+++ app.js
@@ -31,6 +31,26 @@ function getTasks() {
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
+
+  tasks.forEach(function (task) {
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement('a');
+    // add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+  });
 }
 
 // Add task
